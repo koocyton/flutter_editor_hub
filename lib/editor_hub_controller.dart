e10501d92fb988efdedfe884a9dac74ac7cfef0b
liuyi <yi.liu@gameark.cn>
2024-05-21 10:27:27 +0800

    区分动作

--- lib/editor_hub_controller.dart
+++ lib/editor_hub_controller.dart
@@ -2,9 +2,9 @@ import 'package:flutter_editor_hub/editor_hub_widget.dart';
 
 class EditorHubController {
 
-  double keyBottomMargin = 0;
+  double keyboradBottomMargin = 0;
 
-  double optBottomMargin = 0;
+  double panelBottomMargin = 0;
 
   int bottomAnimatedMilliseconds = 0;
 
@@ -16,73 +16,55 @@ class EditorHubController {
     hubState = state;
   }
 
+  void onKeyboardShowing(double bm) {
+    if (panelBottomMargin<bm) {
+      hubState.setState((){
+        bottomAnimatedMilliseconds = 0;
+        panelBottomMargin = bm;
+      });
+    }
+    keyboradBottomMargin = bm;
+  }
+
+  void onKeyboardHiding(double bm) {
+    if (panelBottomMargin>bm) {
+      hubState.setState((){
+        bottomAnimatedMilliseconds = 0;
+        panelBottomMargin = bm;
+      });
+    }
+    keyboradBottomMargin = bm;
+  }
+
   void switchPanelBar() {
-    statusExcutor(
-      onOpHideKbHide:(){
+    statusDispatcher(
+      onPanelHideKbHide:(){
       },
-      onOpShowKbHide:(){
+      onPanelShowKbHide:(){
       },
-      onOpHideKbShow:(){
+      onPanelHideKbShow:(){
       }
     );
   }
 
-  void statusExcutor({Function? onOpHideKbHide, Function? onOpShowKbHide, Function? onOpHideKbShow}) {
+  void statusDispatcher({Function? onPanelHideKbHide, Function? onPanelShowKbHide, Function? onPanelHideKbShow}) {
     // 导航栏在底部 -> 滑出导航栏，键盘不动
-    if (optBottomMargin<=0) {
-      if (onOpHideKbHide!=null) {
-        onOpHideKbHide();
+    if (panelBottomMargin<=0) {
+      if (onPanelHideKbHide!=null) {
+        onPanelHideKbHide();
       }
     }
     // 键盘弹出,导航栏在键盘上
-    else if (optBottomMargin>0 && keyBottomMargin>0) {
-      if (onOpShowKbHide!=null) {
-        onOpShowKbHide();
+    else if (panelBottomMargin>0 && keyboradBottomMargin>0) {
+      if (onPanelShowKbHide!=null) {
+        onPanelShowKbHide();
       }
     }
     // 操作栏已显示(键盘未弹出,导航栏弹出)
-    else if (optBottomMargin>0 && keyBottomMargin<=0) {
-      if (onOpHideKbShow!=null) {
-        onOpHideKbShow();
+    else if (panelBottomMargin>0 && keyboradBottomMargin<=0) {
+      if (onPanelHideKbShow!=null) {
+        onPanelHideKbShow();
       }
     }
   }
-
-  void hideOptboard() {
-    // 导航栏在底部
-    if (bottomMargin==0) {
-
-    }
-    // 键盘弹出,导航栏在键盘上
-    // 操作栏已显示(键盘未弹出,导航栏弹出)
-    hubState.setState(() {
-      bottomAnimatedMilliseconds = 200;
-      bottomMargin = 0.00;
-      Future.delayed(const Duration(milliseconds: 200), (){
-        bottomAnimatedMilliseconds = 0;
-        bottomMargin = 0.00;
-      });
-    });
-  }
-
-  void showOptboard(int idx) {
-    // 导航栏在底部
-    if (bottomMargin==0) {
-      
-    }
-    // 键盘弹出,导航栏在键盘上
-    // 操作栏已显示(键盘未弹出,导航栏弹出)
-    hubState.setState(() {
-      bottomAnimatedMilliseconds = 200;
-      bottomMargin = 322.00;
-      Future.delayed(const Duration(milliseconds: 200), (){
-        bottomAnimatedMilliseconds = 0;
-        bottomMargin = 322.00;
-      });
-    });
-  }
-
-  void hideNavbar() {
-  }
-
 }
